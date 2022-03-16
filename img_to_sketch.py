a5e9248b7164ee25bafb9c10e9d0637964aa508e
pramod-karan <70053892+pramod-karan@users.noreply.github.com>
2022-03-16 15:55:30 +0530

    Add files via upload

---
+++ img_to_sketch.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread("images.jpeg")
+
+
+grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(grey_img)
+blur = cv2.GaussianBlur(invert,(21,21),60)
+invertedblur = cv2.bitwise_not(blur)
+sketch = cv2.divide(grey_img, invertedblur, scale = 256.0)
+
+cv2.imwrite("sketch.png", sketch)
